62552e538b89d38f8a064d75d4e132013d567707
Mbah Emeka <frankemeks77@yahoo.com>
2020-04-12 17:07:25 +0100

    Fix log

--- src/App/Lib/App.php
+++ src/App/Lib/App.php
@@ -12,11 +12,12 @@ class App
         $end_time = microtime(true);
         $method = $_SERVER['REQUEST_METHOD'];
         $path = $_SERVER['REQUEST_URI'];
+
         $time = floor(($end_time - START_TIME)  * 1000);
 
         $time = sprintf("%02d", $time); //prefix with leading zero if single
 
-        $log = "{$method}\t\t{$path}\t\t200\t\t{$time} ms".PHP_EOL;
+        $log = "{$method}\t\t{$path}\t\t200\t\t{$time}\tms".PHP_EOL;
         file_put_contents(BASE_PATH . "/log.txt", $log, FILE_APPEND);
     }
 
